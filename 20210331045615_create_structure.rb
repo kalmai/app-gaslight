ede2adc1a29bc2ac9b7507a4fdf13cd680c41091
kalmai <leozverres@gmail.com>
2021-03-31 21:12:02 -0400

    fixing migrations

---
+++ 20210331045615_create_structure.rb
@@ -0,0 +1,31 @@
+class CreateStructure < ActiveRecord::Migration[6.1]
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
+    t.integer "user_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "comments", "articles"
+end
--- db/migrate/20210331030925_remove_user_from_articles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromArticles < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :articles, :user, :string
-  end
-end
